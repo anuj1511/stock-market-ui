93359b331d77767f97cd3c7185346a0f277eaf8f
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2024-08-30 02:46:50 +0530

    notistack setup done

--- stock-market/app/layout.tsx
+++ stock-market/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from '@/app/ui/utility/NavBar'
-
+import NavBar from "@/app/ui/utility/NavBar";
+import { NotistackProvider } from "./hooks/SnackbarProviderClient";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        {children}
+        <NotistackProvider>
+          <NavBar />
+          {children}
+        </NotistackProvider>
       </body>
     </html>
   );
